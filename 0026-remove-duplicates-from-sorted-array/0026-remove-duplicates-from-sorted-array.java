0d84a57d44baae2354c0b7c0fa8484ef5176be6b
Jung JiSung <jung48182@gmail.com>
2024-12-08 22:25:30 +0900

    Time: 1 ms (66.41%), Space: 45.2 MB (5.25%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 0;
+        for(int num : nums) {
+            if(num != nums[k]) {
+                nums[++k] = num;
+            }
+        }
+        
+        return k+1;
+    }
+}

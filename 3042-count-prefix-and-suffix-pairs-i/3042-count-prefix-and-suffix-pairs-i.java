46e672807c2dc8c2b3caeaafc9c4dc77cd32fcde
Jung JiSung <jung48182@gmail.com>
2025-01-08 21:38:19 +0900

    Time: 3 ms (40.04%), Space: 44.3 MB (21.26%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -1,9 +1,9 @@
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
+        int n = words.length;
         int answer = 0;
-        for(int i=0; i<words.length; i++) {
-            for(int j=i+1; j<words.length; j++) {
-                if(i==j || words[i].length() > words[j].length()) continue;
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
                 if(isPrefixAndSuffix(words[i], words[j])) answer++;
             }
         }
@@ -15,9 +15,7 @@ class Solution {
         int l2 = str2.length();
 
         if(l1 > l2) return false;
-        if(!str1.equals(str2.substring(0, l1))) return false;
-        if(!str1.equals(str2.substring(l2-l1, l2))) return false;
-
-        return true;
+        
+        return str1.equals(str2.substring(0, l1)) && str1.equals(str2.substring(l2-l1));
     }
 }

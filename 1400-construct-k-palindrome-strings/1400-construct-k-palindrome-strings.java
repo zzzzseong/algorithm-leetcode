a2d9bce8df7502354bd0bbb39eebad03b656321f
Jung JiSung <jung48182@gmail.com>
2025-01-11 21:17:20 +0900

    Time: 4 ms (94.6%), Space: 45.4 MB (61.15%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public boolean canConstruct(String s, int k) {
-        if(s.length() < k) return false;
+        int n = s.length();
+
+        if(n < k) return false;
+        if(n == k) return true;
 
         int[] alphabets = new int[26];
-        for(int i=0; i<s.length(); i++) alphabets[s.charAt(i)-'a']++;
+        for(int i=0; i<n; i++) alphabets[s.charAt(i)-'a']++;
 
-        for(int i=0; i<alphabets.length; i++) if(alphabets[i]%2 != 0) k--;
+        for(int i=0; i<26; i++) if(alphabets[i]%2 != 0) k--;
 
         if(k < 0) return false;
         else return true;

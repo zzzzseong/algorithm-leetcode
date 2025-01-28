47eaa5360c05890c19d92044f23d9e0a13db93cf
Jung JiSung <jung48182@gmail.com>
2025-01-28 20:43:55 +0900

    Time: 1 ms (86.18%), Space: 46.3 MB (16.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int minPrice = prices[0];
         
         int answer = 0;
-        for(int i=1; i<prices.length; i++) {
+        int minPrice = prices[0];
+
+        int n = prices.length;
+        for(int i=1; i<n-1; i++) {
             int price = prices[i];
-            if(i == prices.length-1 && price > minPrice) {
-                answer += price-minPrice;
-                break;
-            }
             
             /* if - price[i]가 min보다 작다면 */
             /* min을 price[i]로 바꾼다 */
@@ -17,18 +15,14 @@ class Solution {
             /* if - price[i]가 min 보다 크다면 */
             /* price[i+1]이 price[i]보다 크거나 같다면 > 판매하지 않는다 */
             /* price[i+1]이 price[i]보다 작다면 > 판매한다 > answer에 price[i]-min을 더한다 > min을 price[i+1]로 세팅한다 */
-            if(price > minPrice) {
-                int nextPrice = prices[i+1];
-                
-                // if(nextPrice >= price) //판매하지 않는다.
-                if(nextPrice < price) {
-                    //판매한다.
-                    answer += price - minPrice;
-                    minPrice = nextPrice;
-                }
-                
+            if(price > minPrice && prices[i+1] < price) {
+                answer += price - minPrice;
+                minPrice = prices[i+1];
             }
         }
+
+        /* 마지막날 주식 판매 */
+        if(prices[n-1] > minPrice) answer += prices[n-1]-minPrice;
 
         return answer;
     }

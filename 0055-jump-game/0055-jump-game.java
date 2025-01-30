78c02232c401e66cf7a73e789c2006c6106e190e
Jung JiSung <jung48182@gmail.com>
2025-01-30 13:49:05 +0900

    Time: 1 ms (99.95%), Space: 45.7 MB (69.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,12 +3,8 @@ class Solution {
         
         int dist = 0;
         for(int i=nums.length-2; i>=0; i--) {
-            dist++;
-
             /* if - nums[i] >= dist > 도달 가능. dist를 0으로 변경 > continue */
-            if(nums[i] >= dist) dist = 0;
-
-            /* if - nums[i] < dist > 도달 불가능. */
+            if(nums[i] >= ++dist) dist = 0;
         }
 
         return dist == 0 ? true : false;

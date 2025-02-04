fc23a3ea97ba8dc1efc726d517c9147f80458876
Jung JiSung <jung48182@gmail.com>
2025-02-04 21:12:16 +0900

    Time: 3 ms (22.93%), Space: 46.4 MB (5.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+
+        for(int left=0, right=0; left<n; left++) {
+            while(right < n && nums[right] == 0) right++;
+
+            if(right == n) break;
+
+            if(nums[left] == 0) swap(nums, left, right);
+
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

c5779b87d287fe8bc16934f72d5e206482fa79bd
Jung JiSung <jung48182@gmail.com>
2025-02-11 19:34:18 +0900

    Time: 0 ms (100%), Space: 42.2 MB (53.41%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,22 +1,27 @@
 class Solution {
     private String[] letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     private List<String> answer = new ArrayList<>();
+    private StringBuilder sb = new StringBuilder();
 
     public List<String> letterCombinations(String digits) {
         if(digits.length() == 0) return answer;
-        traversal(digits.split(""), "", 0);
+        traversal(digits, 0);
         return answer;
     }
 
-    public void traversal(String[] digits, String str, int depth) {
-        if(depth >= digits.length) {
-            answer.add(str);
+    public void traversal(String digits, int depth) {
+        if(depth >= digits.length()) {
+            answer.add(sb.toString());
             return;
         }
 
-        String letter = letters[Integer.parseInt(digits[depth])-2];
+        int idx = digits.charAt(depth)-'0';
+
+        String letter = letters[idx-2];
         for(int i=0; i<letter.length(); i++) {
-            traversal(digits, str+letter.charAt(i), depth+1);
+            sb.append(letter.charAt(i));
+            traversal(digits, depth+1);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
